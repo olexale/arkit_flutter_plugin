204188ec876e93d2868622eecf4c9a4f16c203fe
Olexandr Leuschenko <ole@ciklum.com>
2019-06-25 14:17:22 +0300

    Update Face Detection sample

--- example/lib/face_detection_page.dart
+++ example/lib/face_detection_page.dart
@@ -1,6 +1,6 @@
 import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
-// import 'package:vector_math/vector_math_64.dart' as vector;
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class FaceDetectionPage extends StatefulWidget {
   @override
@@ -11,6 +11,9 @@ class _FaceDetectionPageState extends State<FaceDetectionPage> {
   ARKitController arkitController;
   ARKitNode node;
   String anchorId;
+
+  ARKitNode leftEye;
+  ARKitNode rightEye;
 
   @override
   void dispose() {
@@ -46,9 +49,41 @@ class _FaceDetectionPageState extends State<FaceDetectionPage> {
     anchorId = anchor.identifier;
     node = ARKitNode(geometry: faceAnchor.geometry);
     arkitController.add(node, parentNodeName: anchor.nodeName);
+
+    leftEye = _createEye(faceAnchor.leftEyeTransform);
+    arkitController.add(leftEye, parentNodeName: anchor.nodeName);
+    rightEye = _createEye(faceAnchor.rightEyeTransform);
+    arkitController.add(rightEye, parentNodeName: anchor.nodeName);
+  }
+
+  ARKitNode _createEye(Matrix4 transform) {
+    final position = vector.Vector3(
+      transform.getColumn(3).x,
+      transform.getColumn(3).y,
+      transform.getColumn(3).z,
+    );
+    final material = ARKitMaterial(
+      diffuse: ARKitMaterialProperty(color: Colors.yellow),
+    );
+    final sphere = ARKitBox(
+        materials: [material], width: 0.03, height: 0.03, length: 0.03);
+
+    return ARKitNode(geometry: sphere, position: position);
   }
 
   void _handleUpdateAnchor(ARKitAnchor anchor) {
-    arkitController.updateFaceGeometry(node, anchor.identifier);
+    if (anchor is ARKitFaceAnchor) {
+      final ARKitFaceAnchor faceAnchor = anchor;
+      arkitController.updateFaceGeometry(node, anchor.identifier);
+      _updateEye(leftEye, faceAnchor.leftEyeTransform,
+          faceAnchor.blendShapes['eyeBlink_L']);
+      _updateEye(rightEye, faceAnchor.rightEyeTransform,
+          faceAnchor.blendShapes['eyeBlink_R']);
+    }
+  }
+
+  void _updateEye(ARKitNode node, Matrix4 transform, double blink) {
+    final scale = vector.Vector3(1, 1 - blink, 1);
+    node.scale.value = scale;
   }
 }
